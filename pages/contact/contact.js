dc4e07a5575058871267283866842afcdb10b35b
zqc <58931497+2331256903@users.noreply.github.com>
2025-04-22 10:14:42 +0800

    下拉刷新

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -5,14 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
+    colorList: [],
+    // 1. 定义节流阀
+    isloading: false
+  },
 
+  getColors() {
+    // 显示loading效果
+    wx.showLoading({
+      title: '数据加载中...',
+    })
+    // 刚开始调用方法时, 修改节流阀的值为true
+    this.setData({
+      isloading: true
+    })
+    wx.request({
+      url: 'https://applet-base-api-t.itheima.net/api/color',
+      method: 'get',
+      success: ({data: res}) => {
+        this.setData({
+          colorList: [...this.data.colorList, ...res.data]
+        })
+      },
+      complete: () => {
+        wx.hideLoading() // 隐藏loading效果
+        // 调用完成后将节流阀设为false
+        this.setData({
+          isloading: false
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  onLoad() {
+    this.getColors()
   },
 
   /**
@@ -54,7 +83,11 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    // 判断节流阀的值, 若为true组织接
+    if (!this.data.isloading) {
+      // 调用获取随机颜色数据
+      this.getColors()
+    }
   },
 
   /**
